3734cffdc795a1d2a4990c780c72b17bcea9169f
Joe Braley <106336504+joe-braley@users.noreply.github.com>
2024-06-21 11:10:55 -0700

    test

---
+++ python/env.py
@@ -0,0 +1,12 @@
+def main(env_var_name, env_value):
+    github_env = os.getenv("GITHUB_ENV")
+
+    with open(github_env, 'a') as file:
+        file.write(f"{ env_var_name }={ env_value }")
+
+if __name__ == "__main__":
+    main("TEST1", "A")
+    main("TEST2", "B")
+    main("TEST3", "C")
+    main("TEST4", "D")
+    main("TEST5", "E")
